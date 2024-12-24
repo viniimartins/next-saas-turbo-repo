5c417eb5cd74c9660e982ce15af50281ca45f005
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2024-12-24 20:04:27 -0300

    feat: Permissões por cargos

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,12 @@
-import { ability } from '@saas/auth'
+import { defineAbilityFor } from '@saas/auth'
+
+const ability = defineAbilityFor({ role: 'MEMBER' })
 
 const userCanInviteSomeoneElse = ability.can('invite', 'User')
 const userCanDeleteOthersUsers = ability.can('delete', 'User')
 
+const userCannotDeleteOthersUsers = ability.cannot('delete', 'User')
+
 console.log(userCanInviteSomeoneElse)
 console.log(userCanDeleteOthersUsers)
+console.log(userCannotDeleteOthersUsers)
