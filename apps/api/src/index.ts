cad563b3756f4e27c98b7dffc74bc22ce4cd0e07
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2024-12-31 20:38:51 -0300

    feat: Definindo permissões do app

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,9 @@
-import { defineAbilityFor } from '@saas/auth'
+import { defineAbilityFor, projectSchema } from '@saas/auth'
 
-const ability = defineAbilityFor({ role: 'MEMBER' })
+const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-id' })
 
-const userCanInviteSomeoneElse = ability.can('invite', 'User')
-const userCanDeleteOthersUsers = ability.can('delete', 'User')
+const project = projectSchema.parse({ id: 'project-id', ownerId: 'user-id' })
 
-const userCannotDeleteOthersUsers = ability.cannot('delete', 'User')
-
-console.log(userCanInviteSomeoneElse)
-console.log(userCanDeleteOthersUsers)
-console.log(userCannotDeleteOthersUsers)
+console.log(ability.can('get', 'Billing'))
+console.log(ability.can('create', 'Invite'))
+console.log(ability.can('delete', project))
--- packages/auth/src/subjects/billing.ts
+++ packages/auth/src/subjects/billing.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const billingSubject = z.tuple([
-  z.union([z.literal('manage'), z.literal('create'), z.literal('export')]),
+  z.union([z.literal('manage'), z.literal('get'), z.literal('export')]),
   z.literal('Billing'),
 ])
 
