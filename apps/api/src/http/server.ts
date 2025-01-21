f94d6b3b92b2371d86c21fedde211fa6e04e5166
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2025-01-21 16:00:37 -0300

    feat: Rota: Remover membro

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -20,6 +20,8 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { resetPassword } from './routes/auth/reset-password'
 import { getMembers } from './routes/members/get-members'
+import { removeMember } from './routes/members/remove-member'
+import { updateMember } from './routes/members/update-member'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
@@ -92,6 +94,8 @@ app.register(getProjects)
 app.register(updateProject)
 
 app.register(getMembers)
+app.register(updateMember)
+app.register(removeMember)
 
 app.listen({ port: env.SERVER_PORT, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
