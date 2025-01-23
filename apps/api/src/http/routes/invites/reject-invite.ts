62d130254be4dc5a3c3cb21ad560c56247e77d2e
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2025-01-23 12:34:46 -0300

    feat: Rota: Rejeitar convite

--- apps/api/src/http/routes/invites/reject-invite.ts
+++ apps/api/src/http/routes/invites/reject-invite.ts
@@ -7,16 +7,16 @@ import { prisma } from '@/lib/prisma'
 
 import { BadRequestError } from '../_errors/bad-request-error'
 
-export async function acceptInvite(app: FastifyInstance) {
+export async function rejectInvite(app: FastifyInstance) {
   app
     .withTypeProvider<ZodTypeProvider>()
     .register(auth)
     .post(
-      '/invites/:inviteId/accept',
+      '/invites/:inviteId/reject',
       {
         schema: {
           tags: ['Invites'],
-          summary: 'Accept an invite',
+          summary: 'Reject an invite',
           params: z.object({
             inviteId: z.string().uuid(),
           }),
@@ -54,21 +54,11 @@ export async function acceptInvite(app: FastifyInstance) {
           throw new BadRequestError('This invite belongs to another user.')
         }
 
-        await prisma.$transaction([
-          prisma.member.create({
-            data: {
-              userId,
-              organizationId: invite.organizationId,
-              role: invite.role,
-            },
-          }),
-
-          prisma.invite.delete({
-            where: {
-              id: inviteId,
-            },
-          }),
-        ])
+        prisma.invite.delete({
+          where: {
+            id: inviteId,
+          },
+        })
 
         return reply.status(204).send()
       },
