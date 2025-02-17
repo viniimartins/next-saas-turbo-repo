3db8d586ecc51c32576f5be041fa4cb4a614b4a7
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2025-02-17 16:48:02 -0300

    feat(web): utilizando variáveis ambiente

--- apps/web/src/http/api-client.ts
+++ apps/web/src/http/api-client.ts
@@ -1,9 +1,10 @@
+import { env } from '@saas/env'
 import type { CookiesFn } from 'cookies-next'
 import { getCookie } from 'cookies-next'
 import ky from 'ky'
 
 export const api = ky.create({
-  prefixUrl: 'http://localhost:3333',
+  prefixUrl: env.NEXT_PUBLIC_API_URL,
   hooks: {
     beforeRequest: [
       async (request) => {
