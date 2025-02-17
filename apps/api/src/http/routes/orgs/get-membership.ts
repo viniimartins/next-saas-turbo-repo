7365793f0744065185480a14b6ee67b8e98d3de9
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2025-02-17 14:53:18 -0300

    feat(web):obtendo permissões do usuário

--- apps/api/src/http/routes/orgs/get-membership.ts
+++ apps/api/src/http/routes/orgs/get-membership.ts
@@ -23,6 +23,7 @@ export async function getMembership(app: FastifyInstance) {
             200: z.object({
               membership: z.object({
                 id: z.string().uuid(),
+                userId: z.string().uuid(),
                 role: roleSchema,
                 organizationId: z.string().uuid(),
               }),
@@ -39,6 +40,7 @@ export async function getMembership(app: FastifyInstance) {
           membership: {
             id: membership.id,
             role: membership.role,
+            userId: membership.userId,
             organizationId: membership.organizationId,
           },
         }
--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,12 +1,47 @@
+import { defineAbilityFor } from '@saas/auth'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
+import { getMembership } from '@/http/get-membership'
 import { getProfile } from '@/http/get-profile'
 
 export async function isAuthenticated() {
   const cookieStore = await cookies()
 
   return !!cookieStore.get('token')?.value
+}
+
+export async function getCurrentOrg() {
+  const cookieStore = await cookies()
+
+  return cookieStore.get('org')?.value ?? null
+}
+
+export async function getCurrentMembership() {
+  const org = await getCurrentOrg()
+
+  if (!org) {
+    return null
+  }
+
+  const { membership } = await getMembership(org)
+
+  return membership
+}
+
+export async function ability() {
+  const membership = await getCurrentMembership()
+
+  if (!membership) {
+    return null
+  }
+
+  const ability = defineAbilityFor({
+    id: membership.userId,
+    role: membership.role,
+  })
+
+  return ability
 }
 
 export async function auth() {
@@ -22,7 +57,7 @@ export async function auth() {
     const { user } = await getProfile()
 
     return { user }
-  } catch { }
+  } catch {}
 
   redirect('/api/auth/sign-out')
 }
