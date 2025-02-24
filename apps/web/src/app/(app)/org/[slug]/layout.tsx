00c161b84f06a438852dc707276f4e24149baa52
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2025-02-24 11:59:23 -0300

    feat(web): navegação do app por abas

--- apps/web/src/app/(app)/org/[slug]/layout.tsx
+++ apps/web/src/app/(app)/org/[slug]/layout.tsx
@@ -8,12 +8,12 @@ export default async function OrgLayout({
 }>) {
   return (
     <div>
-      <div className="pt-6">
+      <div className="pt-4">
         <Header />
         <Tabs />
       </div>
 
-      <main className="mx-auto w-full max-w-[1200px] p-4">{children}</main>
+      <main className="mx-auto w-full max-w-[1200px] py-4">{children}</main>
     </div>
   )
 }
