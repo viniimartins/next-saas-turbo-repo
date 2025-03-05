8efabe6de3abafee0ccba421f2c6f58cf59c06fc
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2025-03-05 11:25:51 -0300

    feat(web): listagem de projetos

--- apps/web/src/app/(app)/org/[slug]/(projects)/project-list.tsx
+++ apps/web/src/app/(app)/org/[slug]/(projects)/project-list.tsx
@@ -1,5 +1,8 @@
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 import { ArrowRight } from 'lucide-react'
 
+import { getCurrentOrg } from '@/auth/auth'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import {
@@ -9,37 +12,45 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { getProjects } from '@/http/get-projects'
+
+dayjs.extend(relativeTime)
 
 export default async function ProjectList() {
+  const currentOrg = await getCurrentOrg()
+  const { projects } = await getProjects(currentOrg!)
+
   return (
     <div className="grid grid-cols-3 gap-4">
-      <Card>
-        <CardHeader>
-          <CardTitle>Projeto 01</CardTitle>
-          <CardDescription className="line-clamp-2 leading-relaxed">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
-            magnam, incidunt consectetur, esse beatae neque consequuntur natus
-            voluptates voluptatibus repudiandae unde porro illo? Alias
-            voluptatem itaque quasi! Ullam, possimus blanditiis.
-          </CardDescription>
-        </CardHeader>
-        <CardFooter className="itens flex gap-1.5">
-          <Avatar className="size-4">
-            <AvatarImage src="https://github.com/diego3g.png" />
-            <AvatarFallback />
-          </Avatar>
+      {projects.map(({ id, name, description, owner, createdAt }) => {
+        return (
+          <Card key={id} className="flex flex-col justify-between">
+            <CardHeader>
+              <CardTitle className="text-xl font-medium">{name}</CardTitle>
+              <CardDescription className="line-clamp-2 leading-relaxed">
+                {description}
+              </CardDescription>
+            </CardHeader>
+            <CardFooter className="itens flex gap-1.5">
+              <Avatar className="size-4">
+                {owner.avatarUrl && <AvatarImage src={owner.avatarUrl} />}
+                <AvatarFallback />
+              </Avatar>
 
-          <span className="text-xs text-muted-foreground">
-            Created by{' '}
-            <span className="font-medium text-foreground">Diego Fernandes</span>{' '}
-            a day ago
-          </span>
+              <span className="truncate text-xs text-muted-foreground">
+                <span className="font-medium text-foreground">
+                  {owner.name}
+                </span>
+                {dayjs(createdAt).fromNow()}
+              </span>
 
-          <Button size="xs" variant="outline" className="ml-auto">
-            View <ArrowRight className="ml-2 size-3" />
-          </Button>
-        </CardFooter>
-      </Card>
+              <Button size="xs" variant="outline" className="ml-auto">
+                View <ArrowRight className="ml-2 size-3" />
+              </Button>
+            </CardFooter>
+          </Card>
+        )
+      })}
     </div>
   )
 }
