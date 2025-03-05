cf4f3525ad13f66987ee39e764c008d4b89fefcf
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2025-03-05 15:16:47 -0300

    feat(web): action: aceitar convite

--- apps/web/src/app/api/auth/callback/route.ts
+++ apps/web/src/app/api/auth/callback/route.ts
@@ -1,6 +1,7 @@
 import { cookies } from 'next/headers'
 import { type NextRequest, NextResponse } from 'next/server'
 
+import { acceptInvite } from '@/http/accept-invite'
 import { signInWithGithub } from '@/http/sign-in-with-github'
 
 export async function GET(request: NextRequest) {
@@ -23,6 +24,16 @@ export async function GET(request: NextRequest) {
     path: '/',
     maxAge: 60 * 60 * 24 * 7, // 7days
   })
+
+  const inviteId = cookieStore.get(`inviteId`)?.value
+
+  if (inviteId) {
+    try {
+      await acceptInvite(inviteId)
+      cookieStore.delete(`inviteId`)
+      // eslint-disable-next-line prettier/prettier
+    } catch { }
+  }
 
   const redirectUrl = request.nextUrl.clone()
 
--- apps/web/src/app/auth/sign-in/actions.ts
+++ apps/web/src/app/auth/sign-in/actions.ts
@@ -4,6 +4,7 @@ import { HTTPError } from 'ky'
 import { cookies } from 'next/headers'
 import { z } from 'zod'
 
+import { acceptInvite } from '@/http/accept-invite'
 import { signInWithPassword } from '@/http/sign-in-with-password'
 
 const signInSchema = z.object({
@@ -36,6 +37,17 @@ export async function signInWithEmailAndPassword(data: FormData) {
       path: '/',
       maxAge: 60 * 60 * 24 * 7, // 7 days
     })
+
+    const inviteId = cookieStore.get(`inviteId`)?.value
+
+    if (inviteId) {
+      try {
+        await acceptInvite(inviteId)
+
+        cookieStore.delete(`inviteId`)
+        // eslint-disable-next-line prettier/prettier
+      } catch { }
+    }
   } catch (err) {
     if (err instanceof HTTPError) {
       const { message } = await err.response.json()
